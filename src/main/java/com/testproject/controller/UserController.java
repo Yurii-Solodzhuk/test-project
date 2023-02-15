5ae89ff16698e949f57275e2434dcb63fdc4a7d1
Yurii Solodzhuk <yurii.solodzhuk@devcom.com>
2023-02-15 03:04:41 +0200

    Created list of users page with admin's features and edit user page

--- src/main/java/com/testproject/controller/UserController.java
+++ src/main/java/com/testproject/controller/UserController.java
@@ -1,24 +1,29 @@
 package com.testproject.controller;
 
+import com.testproject.model.Role;
 import com.testproject.model.User;
 import com.testproject.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/user")
-@PreAuthorize("hasAuthority('ADMIN')")
+//@PreAuthorize("hasAuthority('ADMIN')")
 public class UserController {
 
     @Autowired
     private UserRepository userRepository;
 
     @GetMapping
-    public String getUserList(Model model) {
+    public String getUserList(@AuthenticationPrincipal User currentUser,
+                              Model model) {
         model.addAttribute("users", userRepository.findAll());
+        boolean isAdmin = currentUser.getRoles().contains(Role.ADMIN);
+        model.addAttribute("isAdmin", isAdmin);
         return "user-list";
     }
 
@@ -30,12 +35,22 @@ public class UserController {
 
     @PostMapping
     public String saveUser(@RequestParam("userId") User user,
-                           @RequestParam String email,
-                           @RequestParam String firstName) {
+                           @RequestParam String firstName,
+                           @RequestParam String lastName,
+                           @RequestParam String phoneNumber,
+                           @RequestParam String email) {
 
-        user.setEmail(email);
         user.setFirstName(firstName);
+        user.setLastName(lastName);
+        user.setPhoneNumber(phoneNumber);
+        user.setEmail(email);
         userRepository.save(user);
+        return "redirect:/user";
+    }
+
+    @GetMapping("/delete")
+    public String delete(@RequestParam("userId") Long userId){
+        userRepository.deleteById(userId);
         return "redirect:/user";
     }
 }
