7132432bc538d41452b5a2a1e76b03622d5e53af
Yurii Solodzhuk <yurii.solodzhuk@devcom.com>
2023-02-17 12:46:27 +0200

    Added @Query to UserRepository

--- src/main/java/com/testproject/repository/UserRepository.java
+++ src/main/java/com/testproject/repository/UserRepository.java
@@ -4,8 +4,10 @@ import com.testproject.model.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    @Query("SELECT u FROM users u  WHERE u.email = ?1")
     User findUserByEmail(String email);
     Page<User> findAll(Pageable pageable);
 }
